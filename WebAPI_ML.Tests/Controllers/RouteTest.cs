a01f48bcc88f0fd685ac7cde312a41118d236c6a
Julio Makdisse Saito <saitodisse@gmail.com>
2014-05-21 21:55:03 -0300

    a lot more details to call routes web service

--- WebAPI_ML.Tests/Controllers/RouteTest.cs
+++ WebAPI_ML.Tests/Controllers/RouteTest.cs
@@ -11,26 +11,35 @@ namespace WebAPI_ML.Tests.Controllers
         [TestMethod]
         public void CalcRoute()
         {
-            //var controller = new RouteController();
+            var controller = new RouteController();
 
-            //var addressItems = new List<AddressItem>();
-            //addressItems.Add(new AddressItem
-            //{
-            //    street = "Pça. Augusta Vitória, 1000",
-            //    X = -46.5737289,
-            //    Y = -23.4844051
-            //});
+            var addressItems = new List<AddressItem>();
+            addressItems.Add(new AddressItem
+            {
+                street = "Pça. Augusta Vitória, 1000",
+                X = -46.5737289,
+                Y = -23.4844051
+            });
 
-            //addressItems.Add(new AddressItem
-            //{
-            //    street = "Vp. Secundina Augusta Santos, 1000",
-            //    X = -46.579239,
-            //    Y = -23.4790611
-            //});
+            addressItems.Add(new AddressItem
+            {
+                street = "Vp. Secundina Augusta Santos, 1000",
+                X = -46.579239,
+                Y = -23.4790611
+            });
 
-            //var routeInfo = controller.Get(addressItems.ToArray());
+            var routeQueryConfig = new RouteQueryConfig();
+            routeQueryConfig.AddressItens = addressItems;
+            routeQueryConfig.RouteType = RouteType.RotaEvitandoTrânsito;
+            routeQueryConfig.AverageConsumption = 12;
+            routeQueryConfig.TankCapacity = 70;
+            routeQueryConfig.FuelPrice = 1.92;
+            routeQueryConfig.AverageSpeed = 70;
+            routeQueryConfig.TotalFeeCat = TotalFeeCat.AutomóvelCaminhonetaEFurgãodoisEixosSimples;
 
-            //Assert.IsNotNull(routeInfo);
+            var routeInfo = controller.Post(routeQueryConfig);
+
+            Assert.IsTrue(routeInfo.routeTotals.totalDistance > 0);
         }
     }
 }
--- WebAPI_ML/Controllers/RouteController.cs
+++ WebAPI_ML/Controllers/RouteController.cs
@@ -1,26 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 using WebAPI_ML.RouteServiceReference;
 
 namespace WebAPI_ML.Controllers
 {
+    public class RouteQueryConfig
+    {
+        public IList<AddressItem> AddressItens { get; set; }
+        public RouteType RouteType { get; set; }
+        public float AverageConsumption { get; set; }
+        public int TankCapacity { get; set; }
+        public double FuelPrice { get; set; }
+        public int AverageSpeed { get; set; }
+        public TotalFeeCat TotalFeeCat { get; set; }
+    }
+    
     public class AddressItem
     {
         public string street { get; set; }
         public double X { get; set; }
         public double Y { get; set; }
     }
-    
+
+    public enum RouteType
+    {
+        RotaPadrãoMaisrápida = 0,
+        RotaPadrãoMaiscurta = 1,
+        RotaAPéPedestre = 2,
+        Rotaevitandobalsas = 6,
+        RotaevitandoestradasdeTerra = 7,
+        RotaevitandobalsaseEstradasDeTerra = 8,
+        RotaevitandoestradasemCondiçõesPrecárias = 9,
+        Rotaevitandopedágios = 10,
+        RotaComtransportePúblico = 21,
+        RotaEvitandoTrânsito = 23,
+        RotapadrãomaisrápidaComOTempoConsiderandoTrânsito = 24,
+        RotapadrãomaiscurtaComOTempoConsiderandoTrânsito = 25,
+        RotapriorizandocicloviasCiclofaixasCiclorrotas = 26,
+    }
+
+    public enum TotalFeeCat
+    {
+        NãoSeráCalculadoOValorDoPedágio = 0,
+        MotocicletasMotonetasEBicicletasAMotor = 1,
+        AutomóvelCaminhonetaEFurgãodoisEixosSimples = 2,
+        AutomóvelCaminhonetaComSemiReboquetrêsEixosSimples = 3,
+        AutomóvelCaminhonetaComReboquequatroEixosSimples = 4,
+        ÔnibusdoisEixosDuplos = 5,
+        ÔnibusComReboquetrêsEixosDuplos = 6,
+        CaminhãoLeveFurgãoECavaloMecânicodoisEixosDuplos = 7,
+        CaminhãoCaminhãoTratorECavaloMecânicoComSemiReboquetrêsEixosDuplos = 8,
+        CaminhãoComReboqueECavaloMecânicoComSemiReboquequatroEixosDuplos = 9,
+        CaminhãoComReboqueECavaloMecânicoComSemiReboquecincoEixosDuplos= 10,
+        CaminhãoComReboqueECavaloMecânicoComSemiReboqueseisEixosDuplos= 11,
+        CaminhãoComReboqueECavaloMecânicoComSemiReboqueseteEixosDuplos= 12,
+        CaminhãoComReboqueECavaloMecânicoComSemiReboqueoitoEixosDuplos= 13,
+        CaminhãoComReboqueECavaloMecânicoComSemiReboquenoveEixosDuplos= 14,
+    }
+
     public class RouteController : ApiController
     {
-        public RouteInfo Post(IList<AddressItem> addresses)
+        public RouteInfo Post(RouteQueryConfig routeQueryConfig)
         {
             const string token = "c13iyCvmcC9mzwkLd0LCbmYC5mUF5m2jNGNtNGt6NmK6NJK=";
 
             var routes = new List<RouteStop>();
-            foreach (var address in addresses)
+            foreach (AddressItem address in routeQueryConfig.AddressItens)
             {
                 var routeStop = new RouteStop();
                 routeStop.description = address.street;
@@ -36,120 +81,23 @@ namespace WebAPI_ML.Controllers
             var routeOptions = new RouteOptions
             {
                 language = "portugues",
-                routeDetails = new RouteDetails { descriptionType = 0, routeType = 1, optimizeRoute = true },
+                routeDetails = new RouteDetails { descriptionType = 0, routeType = (int)routeQueryConfig.RouteType, optimizeRoute = true },
                 vehicle = new Vehicle
                 {
-                    tankCapacity = 20,
-                    averageConsumption = 9,
-                    fuelPrice = 3,
-                    averageSpeed = 60,
-                    tollFeeCat = 2
+                    tankCapacity = routeQueryConfig.TankCapacity,
+                    averageConsumption = routeQueryConfig.AverageConsumption,
+                    fuelPrice = routeQueryConfig.FuelPrice,
+                    averageSpeed = routeQueryConfig.AverageSpeed,
+                    tollFeeCat = (int)routeQueryConfig.TotalFeeCat
                 }
             };
 
             using (var routeSoapClient = new RouteSoapClient())
             {
-                RouteInfo getRouteResponse = routeSoapClient.getRoute(routes.ToArray(),
+                return routeSoapClient.getRoute(
+                    routes.ToArray(),
                     routeOptions,
                     token);
-
-                Console.WriteLine
-                    (
-                        "RouteId: {0}"
-                        + "\n MapInfo - Url: {1}"
-                        + "\n Extent"
-                        + "\n Xmin: {2}"
-                        + "\n YMin: {3}"
-                        + "\n XMax: {4}"
-                        + "\n YMax: {5}",
-                        getRouteResponse.routeId,
-                        getRouteResponse.MapInfo.url,
-                        getRouteResponse.MapInfo.extent.XMin,
-                        getRouteResponse.MapInfo.extent.YMin,
-                        getRouteResponse.MapInfo.extent.XMax,
-                        getRouteResponse.MapInfo.extent.YMax
-                    );
-
-                List<SegmentDescription> segmentDescriptions = getRouteResponse.segDescription.ToList();
-                segmentDescriptions.ForEach(segmentDescription =>
-                    Console.WriteLine
-                        (
-                            "\n\n SegmentDescription - Command: {0}"
-                            + "\n Description: {1}"
-                            + "\n City: {2}"
-                            + "\n State: {3}"
-                            + "\n TollFeeDetails - Price: {4}"
-                            + "\n PricePerAxle: {5}"
-                            + "\n Distance: {6}"
-                            + "\n CumulativeDistance: {7}"
-                            + "\n Latitude: {8}"
-                            + "\n Longitude: {9}",
-                            segmentDescription.command,
-                            segmentDescription.description,
-                            segmentDescription.city.name,
-                            segmentDescription.city.state,
-                            segmentDescription.tollFeeDetails.price,
-                            segmentDescription.tollFeeDetails.pricePerAxle,
-                            segmentDescription.distance,
-                            segmentDescription.cumulativeDistance,
-                            segmentDescription.point.y,
-                            segmentDescription.point.x
-                        ));
-
-                Console.WriteLine
-                    (
-                        "\n\nRouteTotals - TotalDistance: {0}"
-                        + "\n TotalTime: {1}"
-                        + "\n TotalFuelUsed: {2}"
-                        + "\n TotalTollFeeCost: {3}"
-                        + "\n TotalFuelCost: {4}"
-                        + "\n TotalCost: {5}"
-                        + "\n TaxiFarel1: {6}"
-                        + "\n TaxiFare2: {7}",
-                        getRouteResponse.routeTotals.totalDistance,
-                        getRouteResponse.routeTotals.totalTime,
-                        getRouteResponse.routeTotals.totalFuelUsed,
-                        getRouteResponse.routeTotals.totaltollFeeCost,
-                        getRouteResponse.routeTotals.totalfuelCost,
-                        getRouteResponse.routeTotals.totalCost,
-                        getRouteResponse.routeTotals.taxiFare1,
-                        getRouteResponse.routeTotals.taxiFare2
-                    );
-
-                List<RouteSummary> routeSummaries = getRouteResponse.routeSummary.ToList();
-                routeSummaries.ForEach(routeSummary =>
-                    Console.WriteLine
-                        (
-                            "\n\nRouteSummary - Description: {0}"
-                            + "\n Distance: {1}"
-                            + "\n Point - Longitude: {2}"
-                            + "\n Latitude: {3}"
-                            ,
-                            routeSummary.description,
-                            routeSummary.distance,
-                            routeSummary.point.x,
-                            routeSummary.point.y
-                        ));
-
-                Console.WriteLine
-                    (
-                        "\n\nRoadType - TwoLaneHighway: {0}"
-                        + "\n SecondLaneUnderConstruction: {1}"
-                        + "\n OneLaneRoadway: {2}"
-                        + "\n PavingWorkInProgress: {3}"
-                        + "\n DirtRoad: {4}"
-                        + "\n RoadwayInPoorConditions: {5}"
-                        + "\n Ferry: {6}",
-                        getRouteResponse.roadType.twoLaneHighway,
-                        getRouteResponse.roadType.secondLaneUnderConstruction,
-                        getRouteResponse.roadType.oneLaneRoadway,
-                        getRouteResponse.roadType.pavingWorkInProgress,
-                        getRouteResponse.roadType.dirtRoad,
-                        getRouteResponse.roadType.roadwayInPoorConditions,
-                        getRouteResponse.roadType.ferry
-                    );
-
-                return getRouteResponse;
             }
         }
     }
